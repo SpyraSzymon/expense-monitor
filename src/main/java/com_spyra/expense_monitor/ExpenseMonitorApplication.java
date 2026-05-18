d65e6d7b0e2c5e42e455b957ad110f8828a49c3b
SpyrsOn <szymon.spyra1@gmail.com>
2026-05-18 18:36:22 +0200

    init

---
+++ src/main/java/com_spyra/expense_monitor/ExpenseMonitorApplication.java
@@ -0,0 +1,13 @@
+package com_spyra.expense_monitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpenseMonitorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpenseMonitorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com_spyra/expense_monitor/ExpenseMonitorApplicationTests.java
@@ -0,0 +1,13 @@
+package com_spyra.expense_monitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpenseMonitorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
