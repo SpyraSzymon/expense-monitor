733bfe5a5559264fb683fcc44d93c8d7886617db
SpyrsOn <szymon.spyra1@gmail.com>
2026-05-20 18:36:57 +0200

    @ controller

---
+++ src/main/java/com_spyra/expense_monitor/category/CategoryController.java
@@ -0,0 +1,10 @@
+package com_spyra.expense_monitor.category;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/api/categories")
+@RestController
+public class CategoryController {
+}
