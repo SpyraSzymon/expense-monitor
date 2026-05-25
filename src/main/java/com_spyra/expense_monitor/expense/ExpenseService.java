ea8995942146d3d5058829d5925e092b429c888f
SpyrsOn <szymon.spyra1@gmail.com>
2026-05-25 17:21:43 +0200

    add ser2

--- src/main/java/com_spyra/expense_monitor/expense/ExpenseService.java
+++ src/main/java/com_spyra/expense_monitor/expense/ExpenseService.java
@@ -1,8 +1,11 @@
 package com_spyra.expense_monitor.expense;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 @Service
 public class ExpenseService {
@@ -13,5 +16,15 @@ public class ExpenseService {
     }
 
 
-    public Expense createExpense(BigDecimal amount)
+    public Expense createExpense(BigDecimal amount){
+        if (amount == null || amount.compareTo(BigDecimal.ZERO) <= 0) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Expense must be greater then 0");
+        }
+        Expense expense = new Expense();
+        return expenseRepository.save(expense);
+    }
+    public List <Expense> getExpense(){
+        return expenseRepository.findAll();
+    }
+
 }
